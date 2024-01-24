04e1af272fc8de3a4086111b8d20833c11e18323
Nate Ferrero <nate@tagme.in>
2024-01-24 21:58:48 +0000

    linkedin response types

--- functions/auth-linkedin.ts
+++ functions/auth-linkedin.ts
@@ -75,7 +75,19 @@ export const onRequestGet: PagesFunction<
 
  const linkedInProfile =
   (await linkedInProfileResponse.json()) as
-   | {}
+   | {
+      sub: string
+      email_verified: boolean
+      name: string
+      locale?: {
+       country: string
+       language: string
+      }
+      given_name: string
+      family_name: string
+      email: string
+      picture?: string
+     }
    | {
       serviceErrorCode: number
       message: string
