9bbe7e4a6b70c42921458bd78443e2b885f22aaf
Nate Ferrero <nate@tagme.in>
2024-02-17 12:42:18 -0500

    open graph tag parsing

--- functions/lib/extractTags.ts
+++ functions/lib/extractTags.ts
@@ -7,31 +7,11 @@ export async function extractTags(
  try {
   const response = await fetchWithTimeout(url)
   const html = await response.text()
-
-  let inTag = false
-  let currentTag: string = ''
-  for (let i = 0; i < html.length; i++) {
-   if (html[i] === '<') {
-    inTag = true
-    currentTag = ''
-   } else if (html[i] === '>') {
-    inTag = false
-    const parts = currentTag.split(' ')
-    if (
-     parts[0] === 'meta' &&
-     parts[1]?.startsWith('property="og:')
-    ) {
-     const key = parts[1]
-      .split('=')[1]
-      .slice(1, -1)
-     const content = parts[2]
-      ?.split('=')[1]
-      .slice(1, -1)
-     tags[key] = content
-    }
-   } else if (inTag) {
-    currentTag += html[i]
-   }
+  const tagRegex =
+   /<meta\s+property="og:([^"]+)"\s+content="([^"]*)"\s*\/?>/gi
+  let match: null | string[]
+  while ((match = tagRegex.exec(html))) {
+   tags[match[1].substring(3)] = match[2]
   }
  } catch (e) {
   tags.title = `Error: ${e.message}`
