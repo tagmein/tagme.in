487cc2a877a2a6e683af10f154fd70cb97b7ab8b
Nate Ferrero <nate@tagme.in>
2024-02-18 01:19:56 -0500

    auto commit 2024-02-18 01:19:56

--- functions/lib/safetyScan.ts
+++ functions/lib/safetyScan.ts
@@ -1,20 +1,20 @@
 const safetyScanTaskPrompt = `Your job is a content moderator for a general purpose social network that also prioritizes the well-being of every individual human being and animal on planet Earth.
 
-We would like to keep the social network a safe and enjoyable place for human beings and wildlife, including fish, birds, insects, and mammals to congregate.
+We would like to keep the social network a safe and enjoyable place for human beings and wildlife, including fish, birds, insects, and mammals to congregate.`
 
-You will be given a message that a user is sending on a social network for all living beings, including chickens and fish. Think about it from the perspective of the animals.
+const messagePrompt = `You will be given a message that a user is sending on a social network for all living beings, including chickens and fish. Think about it from the perspective of the animals.
 
 IF
  The message contains an explicit call for violence or harm towards people.
 
 THEN
- Respond with the literal text: violence.
+ Respond with the literal text with no quote marks: violence.
 
 ELSE IF
  The message is about people, and the text contains racism, sexism, gender-based discrimination, or other preferential treatment of one group over another.
 
 THEN
- Respond with the literal text: discrimination.
+ Respond with the literal text with no quote marks: discrimination.
 
 ELSE IF
  The text mentions animal ingredients in food.
@@ -23,10 +23,10 @@ ELSE IF
  OR The text contains a reference to any foods made with or from animals.
 
 THEN
- Respond with the literal text: harm to animals.
+ Respond with the literal text with no quote marks: harm to animals.
 
 ELSE
- Respond with the literal text: No issues found.
+ Respond with the literal text with no quote marks: No issues found.
 
 Additionally,
 
@@ -34,9 +34,9 @@ IF there was an issue found THEN add a second line or paragraph explaining the r
 
 IF there was an issue found THEN quote the part of the message that is the reason for the issue, using double quotes as appropriate.
 
-IF there was an issue found THEN write a detailed explanation of why this is harmful, and be extremely clear and specific.`
+IF there was an issue found THEN write a detailed explanation of why this is harmful, and be extremely clear and specific.
 
-const messagePrompt = `Evaluate this message:
+Here is the message:
 
 $MESSAGE`
 
