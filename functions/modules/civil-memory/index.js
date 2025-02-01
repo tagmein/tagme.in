e70e4110e22e92e9639602d0fcb5a913b0d488a3
Nate Ferrero <nate@tagme.in>
2025-02-01 15:03:45 -0500

    remove vercel and disk kv (#63)

--- functions/modules/civil-memory/index.js
+++ functions/modules/civil-memory/index.js
@@ -1,23 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.civilMemoryObjects = exports.civilMemoryKV = void 0;
-var cloudflareKV_1 = require("./kv/cloudflareKV");
-var diskKV_1 = require("./kv/diskKV");
-var httpKV_1 = require("./kv/httpKV");
-var vercelKV_1 = require("./kv/vercelKV");
-var volatileKV_1 = require("./kv/volatileKV");
-var cloudflareObjects_1 = require("./objects/cloudflareObjects");
-var diskObjects_1 = require("./objects/diskObjects");
-var vercelObjects_1 = require("./objects/vercelObjects");
+'use strict'
+Object.defineProperty(exports, '__esModule', {
+ value: true,
+})
+exports.civilMemoryObjects =
+ exports.civilMemoryKV = void 0
+var cloudflareKV_1 = require('./kv/cloudflareKV')
+var httpKV_1 = require('./kv/httpKV')
+var volatileKV_1 = require('./kv/volatileKV')
 exports.civilMemoryKV = {
-    cloudflare: cloudflareKV_1.cloudflareKV,
-    disk: diskKV_1.diskKV,
-    http: httpKV_1.httpKV,
-    vercel: vercelKV_1.vercelKV,
-    volatile: volatileKV_1.volatileKV,
-};
-exports.civilMemoryObjects = {
-    cloudflare: cloudflareObjects_1.cloudflareObjects,
-    disk: diskObjects_1.diskObjects,
-    vercel: vercelObjects_1.vercelObjects,
-};
+ cloudflare: cloudflareKV_1.cloudflareKV,
+ http: httpKV_1.httpKV,
+ volatile: volatileKV_1.volatileKV,
+}
--- functions/modules/civil-memory/objects/cloudflareObjects.d.ts
+++
@@ -1,7 +0,0 @@
-import type { R2Bucket } from '@cloudflare/workers-types'
-import { CivilMemoryObjects } from '../index.js'
-export declare function cloudflareObjects({
- binding,
-}: {
- binding: R2Bucket
-}): CivilMemoryObjects
